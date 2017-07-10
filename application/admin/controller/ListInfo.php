1fd001cdf9fb450043004a2079572b4b7215d1ed
lgy <764448863@qq.com>
2017-07-10 10:32:35 +0800

    20170710 换了一个模板感觉还是用api来做吧 哎

---
+++ application/admin/controller/ListInfo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/7/10
+ * Time: 9:20
+ */
+
+namespace app\admin\controller;
+
+
+class ListInfo
+{
+
+}
--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,50 +1,98 @@
-/*
- * @Author: Paco
- * @Date:   2017-02-07
- * @lastModify 2017-03-19
- * +----------------------------------------------------------------------
- * | jqadmin [ jq酷打造的一款懒人后台模板 ]
- * | Copyright (c) 2017 http://jqadmin.jqcool.net All rights reserved.
- * | Licensed ( http://jqadmin.jqcool.net/licenses/ )
- * | Author: Paco <admin@jqcool.net>
- * +----------------------------------------------------------------------
- */
+layui.config({
+	base : "js/"
+}).use(['form','element','layer','jquery'],function(){
+	var form = layui.form(),
+		layer = parent.layer === undefined ? layui.layer : parent.layer,
+		element = layui.element(),
+		$ = layui.jquery;
 
-layui.define(['jquery', 'form', 'layer', 'ajax', 'modal', 'jqmenu'], function(exports) {
-    var $ = layui.jquery,
-        layer = layui.layer,
-        jqmenu = layui.jqmenu,
-        ajax = layui.ajax,
-        modal = layui.modal,
-        menu = new jqmenu(),
-        jqMain = function() {};
+	$(".panel a").on("click",function(){
+		window.parent.addTab($(this));
+	})
 
-    /**
-     *@todo 初始化方法
-     */
-    jqMain.prototype.init = function() {
-        this.panelToggle();
-        modal.init();
-        menu.menuBind();
-    }
+	//动态获取文章总数和待审核文章数量,最新文章
+	$.get("../json/newsList.json",
+		function(data){
+			var waitNews = [];
+			$(".allNews span").text(data.length);  //文章总数
+			for(var i=0;i<data.length;i++){
+				var newsStr = data[i];
+				if(newsStr["newsStatus"] == "待审核"){
+					waitNews.push(newsStr);
+				}
+			}
+			$(".waitNews span").text(waitNews.length);  //待审核文章
+			//加载最新文章
+			var hotNewsHtml = '';
+			for(var i=0;i<5;i++){
+				hotNewsHtml += '<tr>'
+		    	+'<td align="left">'+data[i].newsName+'</td>'
+		    	+'<td>'+data[i].newsTime+'</td>'
+		    	+'</tr>';
+			}
+			$(".hot_news").html(hotNewsHtml);
+		}
+	)
 
-    /**
-     *@todo 绑定面板显示隐藏按钮单击事件
-     */
-    jqMain.prototype.panelToggle = function() {
-        $('.panel-toggle').bind("click", function() {
-            var obj = $(this).parent('.panel-heading').next('.panel-body');
-            if (obj.css('display') == "none") {
-                $(this).find('i').html('&#xe604;');
-                obj.slideDown();
-            } else {
-                $(this).find('i').html('&#xe603;');
-                obj.slideUp();
-            }
-        })
-    }
+	//图片总数
+	$.get("../json/images.json",
+		function(data){
+			$(".imgAll span").text(data.length);
+		}
+	)
 
-    var main = new jqMain();
-    main.init();
-    exports('main', {});
-});
+	//用户数
+	$.get("../json/usersList.json",
+		function(data){
+			$(".userAll span").text(data.length);
+		}
+	)
+
+	//新消息
+	$.get("../json/message.json",
+		function(data){
+			$(".newMessage span").text(data.length);
+		}
+	)
+
+
+	//数字格式化
+	$(".panel span").each(function(){
+		$(this).html($(this).text()>9999 ? ($(this).text()/10000).toFixed(2) + "<em>万</em>" : $(this).text());	
+	})
+
+	//系统基本参数
+	if(window.sessionStorage.getItem("systemParameter")){
+		var systemParameter = JSON.parse(window.sessionStorage.getItem("systemParameter"));
+		fillParameter(systemParameter);
+	}else{
+		$.ajax({
+			url : "../json/systemParameter.json",
+			type : "get",
+			dataType : "json",
+			success : function(data){
+				fillParameter(data);
+			}
+		})
+	}
+
+	//填充数据方法
+ 	function fillParameter(data){
+ 		//判断字段数据是否存在
+ 		function nullData(data){
+ 			if(data == '' || data == "undefined"){
+ 				return "未定义";
+ 			}else{
+ 				return data;
+ 			}
+ 		}
+ 		$(".version").text(nullData(data.version));      //当前版本
+		$(".author").text(nullData(data.author));        //开发作者
+		$(".homePage").text(nullData(data.homePage));    //网站首页
+		$(".server").text(nullData(data.server));        //服务器环境
+		$(".dataBase").text(nullData(data.dataBase));    //数据库版本
+		$(".maxUpload").text(nullData(data.maxUpload));    //最大上传限制
+		$(".userRights").text(nullData(data.userRights));//当前用户权限
+ 	}
+
+})
